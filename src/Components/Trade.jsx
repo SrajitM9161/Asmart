cbe3e6834537695475758ef54dd3f6d16be4a2a2
Srajit Mishra <neetuuno123@gmail.com>
2024-01-24 23:51:21 +0530

    LOGO and File completed

---
+++ src/Components/Trade.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trade = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Trade
---
+++ src/Routings/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../CSS/NavBar.css"
+const NavBar = () => {
+  return (
+    <>
+      <h1>hii</h1>
+    </>
+  )
+}
+
+export default NavBar
