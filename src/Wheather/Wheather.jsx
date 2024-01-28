1e4bf5b9beddf4523b973cc0af7abe81bf073e0f
Srajit Mishra <neetuuno123@gmail.com>
2024-01-28 16:10:17 +0530

    Wheater complete2

--- src/Wheather/Wheather.jsx
+++ src/Wheather/Wheather.jsx
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import API from '../API/oneCall_Api.config'
+import API from '../API/oneCall_Api.config';
+
 const Weather = () => {
   const [emoji, setEmoji] = useState('');
   const [minTemp, setMinTemp] = useState(null);
   const [maxTemp, setMaxTemp] = useState(null);
+  const [location, setLocation] = useState({ city: '', country: '' });
 
   useEffect(() => {
-   
     const getGeolocation = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const { latitude, longitude } = position.coords;
-            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API}`;
-         
-            axios.get(apiUrl)
+            const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API}`;
+
+            axios.get(weatherApiUrl)
               .then(response => {
                 const iconCode = response.data.weather[0].icon;
                 const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
                 setEmoji(iconUrl);
+
                 const minTemperature = response.data.main.temp_min;
                 const maxTemperature = response.data.main.temp_max;
                 setMinTemp(Math.round(minTemperature - 273.15)); 
                 setMaxTemp(Math.round(maxTemperature - 273.15));
+
+                // Extract city and country directly from OpenWeatherMap API response
+                const city = response.data.name || '';
+                const country = response.data.sys.country || '';
+
+                setLocation({ city, country });
               })
               .catch(error => {
                 console.error('Error fetching weather data:', error);
@@ -38,11 +46,16 @@ const Weather = () => {
         console.error('Geolocation is not supported by this browser.');
       }
     };
+
     getGeolocation();
   }, []);
 
   return (
     <div className="weather-info">
+      <div className="location">
+
+        <p>{`${location.city}, ${location.country}`}</p>
+      </div>
       {emoji && <img src={emoji} alt="Weather Icon" />}
       {minTemp !== null && maxTemp !== null && (
         <p>
