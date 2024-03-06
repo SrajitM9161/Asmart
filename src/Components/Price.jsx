68a93843f3520a84c2f3f00a17d5838165761bb1
Dikshabb98 <98941422+Dikshabb98@users.noreply.github.com>
2024-03-07 02:34:09 +0530

    footer add

--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import PAPI from '../PriceAPI/PAPI'
+import Footer from '../Components/Footer';
 const Price = () => {
   return (
     <div>
     <PAPI/>
-      
+      <Footer/>
     </div>
   )
 }
