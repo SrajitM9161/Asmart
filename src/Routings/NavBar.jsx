51ecc249315bd3904d91e7ebc090f72e1b3c2808
Vaibhav <vibhuchauhan37@gmail.com>
2024-03-10 23:38:24 +0530

    User display at homepage and Logout button

--- src/Routings/NavBar.jsx
+++ src/Routings/NavBar.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "../CSS/NavBar.css"
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Weather from '../Wheather/Wheather'
+import {auth} from '../Firebase/config';  
+import {signOut} from "firebase/auth";
 
 const NavBar = () => {
 
   const [isMobile, setIsMobile] = useState(false);
   const [sign, setSign] = useState("signup");
+  const [currUser,setCurrUser]=useState(null);
+
+  
+  useEffect(()=>{
+      const unsubscribe = auth.onAuthStateChanged(user => {
+        setCurrUser(user.email);
+      });
+      return()=>unsubscribe();
+  },[]);
+
+  const logoutButton=async()=>{
+    try{
+        await signOut(auth);
+        alert("Logout successfull");
+        setCurrUser(null);
+        }catch(err){
+            alert(err);
+        }
+ }
+
 
   const toggleMobileNav = () => {
     setIsMobile(!isMobile);
-    if (sign == "signup") {
+    if (sign == "signup") { 
       setSign("sign");
     } else {
       setSign("signup");
     }
   };
+  
 
   return (
     <div className={`navbar ${isMobile ? 'mobile' : ''}`}>
@@ -43,7 +66,7 @@ const NavBar = () => {
           <hr className='menu-opt-hr'/>
           <Link to="/cart">Soil Detction</Link>
           <hr className='menu-opt-hr'/>
-          <Link className='menu-btnComp' to="/login"><button className="btnComp">Login</button></Link>
+          {currUser?(<><p className='currUser'>{currUser}</p><button className='btnComp out' onClick={logoutButton}>Logout</button></>):(<Link className='menu-btnComp' to="/login"><button className="btnComp">Login</button></Link>)}
 
         </div>
 
