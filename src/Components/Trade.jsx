38c1ed76ceee5062696ec94dc167043eb2a92a61
Srajit Mishra <neetuuno123@gmail.com>
2024-03-29 12:23:03 +0530

    Bug Fix

--- src/Components/Trade.jsx
+++ src/Components/Trade.jsx
@@ -28,7 +28,7 @@ const Trade=()=> {
 
     // Getting data from api and setting the state lists.
     useEffect(() => {
-        axios.get("https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd0000012a9b2a370e5b48de5a24f54307cc98e3&format=json&limit=10000")
+        axios.get("")
             .then(response => {
                 const data = response.data.records;
                 setRecords(data);
