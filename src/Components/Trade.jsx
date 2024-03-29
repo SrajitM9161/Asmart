2b0b9dd32731fb159462d6678c5b38349638a36a
Srajit Mishra <neetuuno123@gmail.com>
2024-03-29 13:58:11 +0530

    Bug fix of Contact

--- src/Components/Trade.jsx
+++ src/Components/Trade.jsx
@@ -28,7 +28,7 @@ const Trade=()=> {
 
     // Getting data from api and setting the state lists.
     useEffect(() => {
-        axios.get("https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd00000107640ee53c3249cd401d55a7bfcaae75&format=json&limit=10000")
+        axios.get("")
             .then(response => {
                 const data = response.data.records;
                 setRecords(data);
