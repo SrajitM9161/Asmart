8e936633e41447e1a5fa6dbe30350f5cf8fa2dd1
Srajit Mishra <neetuuno123@gmail.com>
2024-04-03 13:40:16 +0530

    Contact

--- src/projectApiKey/apiKey.js
+++ src/projectApiKey/apiKey.js
@@ -1,3 +1,3 @@
 export const weatherApiKey = 'af49e26b89e9c7dfd06a72bd288021b2';
-const urlWithApiKey = 'https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=ENTER_YOUR_API_KEY&format=json&limit=10000'
+const urlWithApiKey = 'https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=&format=json&limit=10000'
 export default urlWithApiKey;
