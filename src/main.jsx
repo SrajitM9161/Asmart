bede2ff63fdad91a8179ea0ecd539d52f526262a
Samridhi-Chaudhary <samridhi.chaudhary16@gmail.com>
2024-05-25 15:06:01 +0530

    MarketToast

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import { ToastContainer } from 'react-toastify';
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <ToastContainer/> */}
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+        ,
 )
